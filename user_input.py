4a609cb3d4a3bf3890ef94265e1edcec46140c8a
Mohwane Karabo <mashapakarabo9@gmail.com>
2024-05-23 11:34:10 +0200

    first commit

---
+++ user_input.py
@@ -0,0 +1,14 @@
+
+
+def promptUser() :  #function detlaration and  Initialization
+ #{
+ name = input("Enter your name: ")
+ age = input("Enter your Age :")
+ location = input("Enter your Loaction :")
+ user="Hello "+ name +", you are "+ age + " years old and you live in " + location
+ print(user)
+#}
+
+
+promptUser() #calling promptUser function
+
